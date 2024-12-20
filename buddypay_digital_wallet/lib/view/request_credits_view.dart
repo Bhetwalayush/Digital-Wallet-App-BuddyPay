c0f2da3b38f54d1dbdf134b4380734ece02dbf40
BhetwalAyush <rujanbhetwal@gmail.com>
2024-12-20 18:11:03 +0545

    Creating the necessary pages

---
+++ buddypay_digital_wallet/lib/view/request_credits_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RequestCredits extends StatelessWidget {
+  const RequestCredits({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Request credits Page"),
+    );
+  }
+}
---
+++ buddypay_digital_wallet/lib/view/send_credits_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SendCredits extends StatelessWidget {
+  const SendCredits({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Send Credits Page"),
+    );
+  }
+}
