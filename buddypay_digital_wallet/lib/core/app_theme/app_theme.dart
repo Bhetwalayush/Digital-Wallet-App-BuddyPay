7c9c899b4ce18ad817eac519ac71cd686e579fc6
BhetwalAyush <rujanbhetwal@gmail.com>
2024-12-20 17:29:36 +0545

    Adding app_theme

--- buddypay_digital_wallet/lib/core/app_theme/app_theme.dart
+++ buddypay_digital_wallet/lib/core/app_theme/app_theme.dart
@@ -1,5 +1,24 @@
 import 'package:flutter/material.dart';
 
 ThemeData getApplicationTheme() {
-  return ThemeData(primarySwatch: Colors.teal);
+  return ThemeData(
+    primarySwatch: Colors.teal,
+    scaffoldBackgroundColor: const Color(0xff304F50),
+    fontFamily: 'Montserrat Regular',
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        textStyle: const TextStyle(
+            fontSize: 18,
+            // color: Colors.teal,
+            fontWeight: FontWeight.w500,
+            fontFamily: 'Montserrat Bold'),
+        backgroundColor: const Color(0xFF00C9A7),
+        foregroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+          side: const BorderSide(color: Colors.white),
+        ),
+      ),
+    ),
+  );
 }
