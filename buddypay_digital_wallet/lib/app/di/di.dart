b248ebf96602eb3d842bc62c8474906302419983
BhetwalAyush <rujanbhetwal@gmail.com>
2025-01-17 20:51:58 +0545

    hive_service

---
+++ buddypay_digital_wallet/lib/app/di/di.dart
@@ -0,0 +1,79 @@
+import 'package:buddypay_digital_wallet/core/network/hive_service.dart';
+import 'package:buddypay_digital_wallet/features/auth/data/data_source/auth_local_datasource/auth_local_datasource.dart';
+import 'package:buddypay_digital_wallet/features/auth/data/repository/auth_local_repository/auth_local_repository.dart';
+import 'package:buddypay_digital_wallet/features/auth/domain/use_case/login_usecase.dart';
+import 'package:buddypay_digital_wallet/features/auth/domain/use_case/signup_user_usecase.dart';
+import 'package:buddypay_digital_wallet/features/auth/presentation/viewmodels/bloc/login/login_bloc.dart';
+import 'package:buddypay_digital_wallet/features/auth/presentation/viewmodels/bloc/signup/signup_bloc.dart';
+import 'package:buddypay_digital_wallet/features/landing_page/cubit/landing_page_cubit.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+Future<void> initDependencies() async {
+  // First initialize hive service
+  await _initHiveService();
+
+  await _initLandingPageDependencies();
+  await _initRegisterDependencies();
+  await _initLoginDependencies();
+
+  // await _initSplashScreenDependencies();
+}
+
+_initHiveService() {
+  getIt.registerLazySingleton<HiveService>(() => HiveService());
+}
+
+_initRegisterDependencies() {
+  // init local data source
+  getIt.registerLazySingleton(
+    () => AuthLocalDataSource(getIt<HiveService>()),
+  );
+
+  // init local repository
+  getIt.registerLazySingleton(
+    () => AuthLocalRepository(getIt<AuthLocalDataSource>()),
+  );
+
+  // register use usecase
+  getIt.registerLazySingleton<RegisterUseCase>(
+    () => RegisterUseCase(
+      getIt<AuthLocalRepository>(),
+    ),
+  );
+
+  getIt.registerLazySingleton<SignupBloc>(
+    () => SignupBloc(
+      registerUseCase: getIt(),
+    ),
+  );
+}
+
+_initLandingPageDependencies() async {
+  getIt.registerLazySingleton<LandingPageCubit>(
+    () => LandingPageCubit(),
+  );
+}
+
+_initLoginDependencies() async {
+  getIt.registerLazySingleton<LoginUseCase>(
+    () => LoginUseCase(
+      getIt<AuthLocalRepository>(),
+    ),
+  );
+
+  getIt.registerFactory<LoginBloc>(
+    () => LoginBloc(
+      signupBloc: getIt<SignupBloc>(),
+      landingpageCubit: getIt<LandingPageCubit>(),
+      loginUseCase: getIt<LoginUseCase>(),
+    ),
+  );
+}
+
+// _initSplashScreenDependencies() async {
+//   getIt.registerFactory<SplashCubit>(
+//     () => SplashCubit(getIt<LoginBloc>()),
+//   );
+// }
---
+++ buddypay_digital_wallet/lib/core/network/hive_service.dart
@@ -0,0 +1,51 @@
+import 'package:buddypay_digital_wallet/app/constants/hive_table_constants.dart';
+import 'package:buddypay_digital_wallet/features/auth/data/models/auth_hive_model.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:path_provider/path_provider.dart';
+
+class HiveService {
+  static Future<void> init() async {
+    // Initialize the database
+    var directory = await getApplicationDocumentsDirectory();
+    var path = '${directory.path}buddy_pay.db';
+
+    Hive.init(path);
+    // ignore: avoid_print
+    print("Database path: $path");
+    // Register Adapters
+    Hive.registerAdapter(AuthHiveModelAdapter());
+  }
+
+  // Auth Queries
+  Future<void> register(AuthHiveModel auth) async {
+    var box = await Hive.openBox<AuthHiveModel>(HiveTableConstant.userBox);
+    await box.put(auth.userId, auth);
+  }
+
+  // Future<void> deleteAuth(String id) async {
+  //   var box = await Hive.openBox<AuthHiveModel>(HiveTableConstant.userBox);
+  //   await box.delete(id);
+  // }
+
+  // Future<List<AuthHiveModel>> getAllAuth() async {
+  //   var box = await Hive.openBox<AuthHiveModel>(HiveTableConstant.userBox);
+  //   return box.values.toList();
+  // }
+
+  // Login using username and password
+  Future<AuthHiveModel?> login(String phone, String password) async {
+    var box = await Hive.openBox<AuthHiveModel>(HiveTableConstant.userBox);
+    var user = box.values.firstWhere(
+        (element) => element.phone == phone && element.password == password);
+    box.close();
+    return user;
+  }
+
+  Future<void> clearAll() async {
+    await Hive.deleteBoxFromDisk(HiveTableConstant.userBox);
+  }
+
+  Future<void> close() async {
+    await Hive.close();
+  }
+}
