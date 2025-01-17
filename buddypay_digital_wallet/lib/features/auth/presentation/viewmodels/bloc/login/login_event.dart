672ec0b2035fc99cf31ec37947b485263a2540dc
BhetwalAyush <rujanbhetwal@gmail.com>
2025-01-17 20:55:39 +0545

    login bloc

---
+++ buddypay_digital_wallet/lib/features/auth/presentation/viewmodels/bloc/login/login_event.dart
@@ -0,0 +1,40 @@
+part of 'login_bloc.dart';
+
+sealed class LoginEvent extends Equatable {
+  const LoginEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+class NavigateRegisterScreenEvent extends LoginEvent {
+  final BuildContext context;
+  final Widget destination;
+
+  const NavigateRegisterScreenEvent({
+    required this.context,
+    required this.destination,
+  });
+}
+
+class NavigateHomeScreenEvent extends LoginEvent {
+  final BuildContext context;
+  final Widget destination;
+
+  const NavigateHomeScreenEvent({
+    required this.context,
+    required this.destination,
+  });
+}
+
+class LoginUserEvent extends LoginEvent {
+  final String phone;
+  final String password;
+  final BuildContext context;
+
+  const LoginUserEvent({
+    required this.phone,
+    required this.password,
+    required this.context,
+  });
+}
