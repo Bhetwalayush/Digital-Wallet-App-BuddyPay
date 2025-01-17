7cfafb3b28a1ca0eaa12273623b7a7233652ca80
BhetwalAyush <rujanbhetwal@gmail.com>
2025-01-17 20:54:23 +0545

    repository

---
+++ buddypay_digital_wallet/lib/features/auth/domain/repository/auth_repository.dart
@@ -0,0 +1,13 @@
+import 'package:buddypay_digital_wallet/core/error/failure.dart';
+import 'package:buddypay_digital_wallet/features/auth/domain/entity/auth_entity.dart';
+import 'package:dartz/dartz.dart';
+
+abstract interface class IAuthRepository {
+  Future<Either<Failure, void>> registerUser(AuthEntity user);
+
+  Future<Either<Failure, String>> loginUser(String phone, String password);
+
+  // Future<Either<Failure, String>> uploadProfilePicture(File file);
+
+  Future<Either<Failure, AuthEntity>> getCurrentUser();
+}
