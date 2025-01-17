3ecafaa42516aa5a25666d3eb7ce0c733354f7af
BhetwalAyush <rujanbhetwal@gmail.com>
2025-01-17 20:51:27 +0545

    adding_routes

--- buddypay_digital_wallet/lib/main.dart
+++ buddypay_digital_wallet/lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:buddypay_digital_wallet/app.dart';
+import 'package:buddypay_digital_wallet/app/di/di.dart';
+import 'package:buddypay_digital_wallet/core/network/hive_service.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Initialize Hive Database
+  await HiveService.init();
+
+  await initDependencies();
   runApp(const MyApp());
 }
