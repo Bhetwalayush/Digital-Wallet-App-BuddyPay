f9e0ba737d5f9d64f4115484004d15684de2ef90
BhetwalAyush <rujanbhetwal@gmail.com>
2025-01-17 20:52:29 +0545

    hive_model

---
+++ buddypay_digital_wallet/lib/features/auth/data/data_source/auth_data_source.dart
@@ -0,0 +1,11 @@
+import 'package:buddypay_digital_wallet/features/auth/domain/entity/auth_entity.dart';
+
+abstract interface class IAuthDataSource {
+  Future<String> loginUser(String phone, String password);
+
+  Future<void> registerUser(AuthEntity user);
+
+  Future<AuthEntity> getCurrentUser();
+
+  // Future<String> uploadProfilePicture(File file);
+}
