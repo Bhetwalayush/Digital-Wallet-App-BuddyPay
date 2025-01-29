1bec61486192e97a2e99579fe5c3b09dee84fa7f
BhetwalAyush <rujanbhetwal@gmail.com>
2025-01-29 14:59:47 +0545

    fixing_routes

--- buddypay_digital_wallet/lib/app/app.dart
+++ buddypay_digital_wallet/lib/app/app.dart
@@ -1,18 +1,44 @@
+import 'package:buddypay_digital_wallet/app/di/di.dart';
 import 'package:buddypay_digital_wallet/core/app_theme/app_theme.dart';
+import 'package:buddypay_digital_wallet/features/auth/domain/use_case/login_usecase.dart';
+import 'package:buddypay_digital_wallet/features/auth/domain/use_case/signup_user_usecase.dart';
+import 'package:buddypay_digital_wallet/features/auth/presentation/viewmodels/bloc/login/login_bloc.dart';
+import 'package:buddypay_digital_wallet/features/auth/presentation/viewmodels/bloc/signup/signup_bloc.dart';
+import 'package:buddypay_digital_wallet/features/landing_page/cubit/landing_page_cubit.dart';
 import 'package:buddypay_digital_wallet/routes/app_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'BuddyPay',
-      theme: getApplicationTheme(),
-      initialRoute: AppRoute.splash_view,
-      routes: AppRoute.getAppRoutes(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (_) => getIt<LandingPageCubit>(),
+        ),
+        BlocProvider(
+          create: (context) => SignupBloc(
+            registerUseCase: getIt<RegisterUseCase>(), // Inject RegisterUseCase
+          ),
+        ),
+        BlocProvider(
+          create: (context) => LoginBloc(
+            signupBloc: context.read<SignupBloc>(),
+            landingpageCubit: context.read<LandingPageCubit>(),
+            loginUseCase: getIt<LoginUseCase>(),
+          ),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'BuddyPay',
+        theme: getApplicationTheme(),
+        initialRoute: AppRoute.splash_view,
+        routes: AppRoute.getAppRoutes(),
+      ),
     );
   }
 }
